f5c9e9b890c3dbb4a0f6d5f4d28f30c758da9d97
Alvaro <aom24@alu.ua.es>
2017-06-30 23:48:32 +0200

    se eliminan fuentes

--- src/main/java/com/starwars/model/PeopleWithNoPersonalInfo.java
+++
@@ -1,9 +0,0 @@
-package com.starwars.model;
-
-import org.springframework.data.rest.core.config.Projection;
-
-@Projection(name = "peopleWithNoPersonalInfo", types = People.class)
-public interface PeopleWithNoPersonalInfo {
-    String getName();
-    String getBirthYear();
-}
--- src/main/java/com/starwars/model/PlanetOnlyNameAndPopulation.java
+++
@@ -1,9 +0,0 @@
-package com.starwars.model;
-
-import org.springframework.data.rest.core.config.Projection;
-
-@Projection(name = "planetOnlyNameAndPopulation", types = Planet.class)
-public interface PlanetOnlyNameAndPopulation {
-    String getName();
-    Long getPopulation();
-}
